9655ee0ef6d2e9273096ff183e1100cbfc75f5bc
Jordan Tannenbaum <jordantannen@gmail.com>
2022-05-11 23:14:48 -0400

    Add clear button

--- scripts.js
+++ scripts.js
@@ -1,28 +1,33 @@
 function createGrid(size){
-    // Creates columns 
+    // Creates new col
     for (let i = 0; i < size; i++) {
-        let row = document.createElement("div");
-        document.getElementById("canvas").appendChild(row);
+        let col = document.createElement("div");
+        col.classList.add("col");
+        document.getElementById("canvas").appendChild(col);
 
-        // Creates rows
+        // Creates fills col in
         for (let j = 0; j < size; j++) {
             let x = document.createElement("div");
             x.classList.add("unit");
 
             x.addEventListener("mouseenter", () => x.classList.add("color"));
 
-            row.appendChild(x);
+            col.appendChild(x);
         }
+    }
+}
+
+function removeGrid(){
+    let cols = document.getElementsByClassName("col");
+    // cols.length is updated in real time, screwing with the for loop
+    let totalCols = cols.length;
+
+    for (let i = totalCols - 1; i >= 0; i--) {
+        cols[i].remove();
     }
 }
 
 createGrid(16);
 
-// let x = document.createElement("div");
-// x.classList.add("unit");
-// x.addEventListener("mouseenter", () => {
-//     x.classList.add("color");
-// });
-
-
-// document.getElementById("container").appendChild(x);
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", () => removeGrid());
