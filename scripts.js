636ad4ad09f072c60e78ad46e445c6a92ebce7a5
Jordan Tannenbaum <jordantannen@gmail.com>
2022-05-11 21:02:02 -0400

    Create functional canvas grid

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 function createGrid(size){
+    // Creates columns 
     for (let i = 0; i < size; i++) {
-        let x = document.createElement("div");
-        x.classList.add("unit");
-        x.addEventListener("mouseenter", () => {
-        x.classList.add("color");
-        });
-        document.getElementById("canvas").appendChild(x);
+        let row = document.createElement("div");
+        document.getElementById("canvas").appendChild(row);
+
+        // Creates rows
+        for (let j = 0; j < size; j++) {
+            let x = document.createElement("div");
+            x.classList.add("unit");
+
+            x.addEventListener("mouseenter", () => x.classList.add("color"));
+
+            row.appendChild(x);
+        }
     }
 }
 
